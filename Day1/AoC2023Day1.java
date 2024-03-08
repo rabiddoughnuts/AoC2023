9727738c4c3f921c5ddb68e83630a5dd2d2e07c8
rabiddoughnuts <111888263+rabiddoughnuts@users.noreply.github.com>
2024-03-08 15:26:32 -0500

    Finished Part 1

--- Day1/AoC2023Day1.java
+++ Day1/AoC2023Day1.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 
 class AoC2023Day1 {
     public static void main(String[] args) throws FileNotFoundException {
-        String input = "input.txt";
-        File file = new File(input);
+        //String input = "input.txt";
+        File file = new File("input.txt");
         Scanner inscan = new Scanner(file);
         int total = 0;
         int first = 0;
@@ -13,19 +13,22 @@ class AoC2023Day1 {
         while (inscan.hasNextLine()) {
             String line = inscan.nextLine();
             for (char c : line.toCharArray()) {
-                if (first == 0){
-                    first = c;
-                } else {
-                    second = c;
+                if (Character.isDigit(c)){
+                    if (first == 0){
+                        first = c - '0';
+                    } else {
+                        second = c - '0';
+                    }
                 }
             }
             if (second == 0){
                 second = first;
             }
+            System.out.println(first + " " + second);
             String combine = String.valueOf(first) + String.valueOf(second);
             first = 0;
             second = 0;
-            total += Integer.parseInt(combine);
+            total = total + Integer.parseInt(combine);
         }
         inscan.close();
         System.out.println(total);
