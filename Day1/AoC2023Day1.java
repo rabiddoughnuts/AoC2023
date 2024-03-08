f4c22e175ee47adedcb6dbc45af1b24de70f8461
rabiddoughnuts <111888263+rabiddoughnuts@users.noreply.github.com>
2024-03-08 14:43:40 -0500

    First go at solving

--- Day1/AoC2023Day1.java
+++ Day1/AoC2023Day1.java
@@ -1,5 +1,33 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 class AoC2023Day1 {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws FileNotFoundException {
+        String input = "input.txt";
+        File file = new File(input);
+        Scanner inscan = new Scanner(file);
+        int total = 0;
+        int first = 0;
+        int second = 0;
+        while (inscan.hasNextLine()) {
+            String line = inscan.nextLine();
+            for (char c : line.toCharArray()) {
+                if (first == 0){
+                    first = c;
+                } else {
+                    second = c;
+                }
+            }
+            if (second == 0){
+                second = first;
+            }
+            String combine = String.valueOf(first) + String.valueOf(second);
+            first = 0;
+            second = 0;
+            total += Integer.parseInt(combine);
+        }
+        inscan.close();
+        System.out.println(total);
     }
 }
